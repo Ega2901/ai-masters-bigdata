65b21dee75ae044e89fbd06215419d3c5ae0c91c
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-04-12 21:20:58 +0300

    Update model.py

--- projects/4/model.py
+++ projects/4/model.py
@@ -5,10 +5,28 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import LogisticRegression
 
 
+class Textprep:
+  
+  def __init__(self):
+    self.df = df
+    
+  def text_prep(text):
+    text = str(text).lower()
+    text = re.sub('\s+',' ',text)
+    text = text.strip()
+    return text
+    
+  def preptxt(self):
+    prep_text_udf = udf(text_prep, StringType())
+    df = (self.df).withColumn('prep_text', prep_text_udf("reviewText"))\
+        .filter('prep_text <> ""')
+    return df
+
+Textprep = Textprep().preptxt
 Tokenizer = Tokenizer(inputCol = 'prep_text', outputCol = 'tokens')
 hashingTF = HashingTF(inputCol = 'tokens', outputCol = 'rawFeatures')
 assembler = VectorAssembler(inputCols=['rawFeatures'],
                             outputCol='features')
 lr = LogisticRegression(featuresCol='features',
                         labelCol='overall')
-pipeline = Pipeline(stages = [Tokenizer, hashingTF, assembler, lr])
+pipeline = Pipeline(stages = [Textprep, Tokenizer, hashingTF, assembler, lr])
