7fba6b5ca4b15ee66860c26c6642ab2bd1361dba
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-04-12 22:24:57 +0300

    Update model.py

--- projects/4/model.py
+++ projects/4/model.py
@@ -3,10 +3,9 @@ from pyspark.ml.feature import HashingTF, Tokenizer
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import LogisticRegression
-from pyspark.sql.types import *
-import pyspark.sql.functions as f
 
-Tokenizer = Tokenizer(inputCol = "reviewText", outputCol = 'tokens')
+
+Tokenizer = Tokenizer(inputCol = 'reviewText', outputCol = 'tokens')
 hashingTF = HashingTF(numFeatures=100, binary=True, inputCol = 'tokens', outputCol = 'rawFeatures')
 assembler = VectorAssembler(inputCols=['rawFeatures'],
                             outputCol='features')
