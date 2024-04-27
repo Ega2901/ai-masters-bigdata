cab8fa6da46a38d4d1017278f28aa66e10017ac3
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-04-27 21:28:17 +0300

    Update model.py

--- projects/5mla/model.py
+++ projects/5mla/model.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # Определение списка признаков
 numeric_features = ["if"+str(i) for i in range(1, 13)]
-categorical_features = ["cf"+str(i) for i in range(1, 24)]
+categorical_features = ["cf"+str(i) for i in range(14, 39)]
 fields = ["id", "label"] + numeric_features + categorical_features
 
 # Определение опций чтения данных
