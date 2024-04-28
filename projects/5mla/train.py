a8e043827c8921371f05441710f08ecdf374a062
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-04-29 00:38:13 +0300

    Update train.py

--- projects/5mla/train.py
+++ projects/5mla/train.py
@@ -3,7 +3,7 @@ import sys
 import logging
 import mlflow
 import mlflow.sklearn
-
+from mlflow.models import infer_signature
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -63,6 +63,12 @@ preprocessor = ColumnTransformer(
 
 # Split train/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = model.predict_proba(X_test)
+loss = log_loss(y_test, y_pred)
 
 # Log parameters
 mlflow.log_param("model_param1", model_param1)
@@ -72,28 +78,24 @@ if mlflow.active_run():
     mlflow.end_run()
 
 # Start MLflow run
-with mlflow.start_run() as run:
-    # Define the full pipeline including preprocessing and model
-    model = Pipeline(steps=[
-        ('preprocessor', preprocessor),
-        ('classifier', LogisticRegression())  # You can replace this with your custom model
-    ])
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
 
-    # Train the model
-    model.fit(X_train, y_train)
-    
-    # Evaluate model
-    y_pred = model.predict_proba(X_test)
-    loss = log_loss(y_test, y_pred)
-    
-    # Log metrics
-    mlflow.log_metric("log_loss", loss)
-    
-    logging.info(f"log loss: {loss:.3f}")
+    # Log the loss metric
+    mlflow.log_metric("accuracy", loss)
 
-    # Save the model
-    model_output_path = "MLmodel"
-    dump(model, model_output_path)
+    # Set a tag that we can use to remind ourselves what this run was for
+    mlflow.set_tag("Training Info", "Basic LR model for iris data")
 
-    # Log model output
-    mlflow.log_artifact(model_output_path)
+    # Infer the model signature
+    signature = infer_signature(X_train, lr.predict(X_train))
+
+    # Log the model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        artifact_path="lr_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="tracking-quickstart",
+    )
