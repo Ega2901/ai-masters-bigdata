bb81d1b9b1dc8a3fa80be132af9970db359278e5
Ega2901 <mr.tuzharov@gmail.com>
2024-05-03 22:19:14 +0300

    dddd

--- projects/6/filter.py
+++ projects/6/filter.py
@@ -1,10 +1,19 @@
 #!/opt/conda/envs/dsenv/bin/python
 import os
 import sys
+SPARK_HOME = "/usr/lib/spark3"
+PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
+os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
+os.environ["PYSPARK_DRIVER_PYTHON"]= PYSPARK_PYTHON
+os.environ["SPARK_HOME"] = SPARK_HOME
+PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.5-src.zip"))
+sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip")
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.getOrCreate() 
-spark.sparkContext.setLogLevel('WARN')
+conf = SparkConf()
+conf.set("spark.ui.port", "4099")
+spark = SparkSession.builder.config(conf=conf).appName("Spark SQL").getOrCreate()                
 from pyspark.sql.types import *
 import pyspark.sql.functions as f
 from pyspark.ml.feature import HashingTF, Tokenizer
@@ -26,7 +35,6 @@ def filter_df(df):
     return df2
 
 if __name__ == "__main__":
-    conf = SparkConf()
     input_path = str(sys.argv[1])
     out_path = str(sys.argv[2])
     df = spark.read.json(input_path)
