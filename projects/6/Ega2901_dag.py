9ebc87d7b439d2f379a015c4568e18193d7972e3
Ega2901 <mr.tuzharov@gmail.com>
2024-05-06 14:42:15 +0300

    dddd

--- projects/6/Ega2901_dag.py
+++ projects/6/Ega2901_dag.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.sensors.filesystem import FileSensor
-from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
 base_dir = '{{ dag_run.conf["base_dir"] if dag_run else "" }}'
 pyspark_python = "/opt/conda/envs/dsenv/bin/python"
@@ -10,22 +9,22 @@ pyspark_python = "/opt/conda/envs/dsenv/bin/python"
 with DAG(
         dag_id="Ega2901_dag",
         start_date=datetime(2023, 4, 26),
-        schedule=None,
+        schedule_interval=None,
         catchup=False,
         description="HW6",
-        doc_md = """
+        doc_md="""
         HW6 Big Data !!!
         """,
         tags=["example"],
 ) as dag:
     feature_eng_task = BashOperator(
         task_id='feature_eng_task',
-        bash_command=f'{pyspark_python} {base_dir}/filter.py /datasets/amazon/amazon_extrasmall_train.json Ega2901_train_out',       
+        bash_command=f'{pyspark_python} {base_dir}/filter.py {base_dir}/amazon_extrasmall_train.json {base_dir}/Ega2901_train_out',       
     ) 
 
     download_train_task = BashOperator(
         task_id='download_train_task',
-        bash_command=f'hdfs dfs -get Ega2901_train_out {base_dir}/Ega2901_train_out_local',
+        bash_command=f'hdfs dfs -get {base_dir}/Ega2901_train_out {base_dir}/Ega2901_train_out_local',
     )
 
     train_task = BashOperator(
@@ -42,7 +41,8 @@ with DAG(
 
     predict_task = BashOperator(
         task_id='predict_task',
-        bash_command= f'{pyspark_python} {base_dir}/predict.py Ega2901_test_out {base_dir}/Ega2901_hw6_prediction {base_dir}/6.joblib',
+        bash_command=f'{pyspark_python} {base_dir}/predict.py {base_dir}/Ega2901_test_out {base_dir}/Ega2901_hw6_prediction {base_dir}/6.joblib',
     )
 
     feature_eng_task >> download_train_task >> train_task >> model_sensor >> predict_task
+
