24bfe6e47ac4b8547050b9b68ed5fa24c33a3946
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 02:20:09 +0300

    Update train.sh

--- projects/1a/train.sh
+++ projects/1a/train.sh
@@ -5,4 +5,4 @@ project_number=$1
 training_file_path=$2
 
 # Запуск скрипта обучения
-python3 projects/1a/train.py $project_number $training_file_path
+python projects/1a/train.py $project_number $training_file_path
