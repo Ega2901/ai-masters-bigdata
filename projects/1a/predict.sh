7731d29391d7ddcfdd098ac1b7febe1cdce468d6
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 02:31:44 +0300

    Update predict.sh

--- projects/1a/predict.sh
+++ projects/1a/predict.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd ai-masters-bigdata
-#remove output dataset if exists
-hdfs dfs -rm -r -f -skipTrash predicted.csv
-projects/1a/predict.sh projects/1a/predict.py,1a.joblib /datasets/criteo/train-with-id.txt predicted.csv predict.py
+
+# Параметры
+project_files=$1
+input_file_path=$2
+output_file_path=$3
+predict_script=$4
+
+# Запуск скрипта предсказаний
+/opt/conda/envs/dsenv/bin/python $predict_script $project_files $input_file_path > $output_file_path
