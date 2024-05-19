6ac8feda62fae417963f0b2cca7f44d6d2fb62a4
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 01:10:58 +0300

    Update predict.sh

--- projects/1a/predict.sh
+++ projects/1a/predict.sh
@@ -4,13 +4,9 @@
 # 2nd arg - input path
 # 3rd arg - output path
 # 4th arg - mapper file
-
-HADOOP_EXE=/usr/bin/yarn
-HADOOP_STREAM_JAR=/usr/lib/hadoop-mapreduce/hadoop-streaming.jar
-
-FILES=$1
-INPUT=$2
-OUTPUT=$3
-MAPPER=$4
-
-$HADOOP_EXE jar $HADOOP_STREAM_JAR  -files $FILES -D mapred.reduce.tasks=0 -input $INPUT -output $OUTPUT -mapper $MAPPER
+cd ai-masters-bigdata
+#copy input dataset to HDFS
+hdfs dfs -copyFromLocal /datasets/criteo/train-with-id.txt train-with-id.txt
+#remove output dataset if exists
+hdfs dfs -rm -r -f predicted.csv
+projects/tut1/predict.sh projects/tut1/predict.py,projects/tut1/model.py,tut1.joblib hotels.csv predicted.csv predict.py
