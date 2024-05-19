bf06a096c1d00310e6825b925b7d6fe18ab4db2a
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 01:36:53 +0300

    Update predict.sh

--- projects/1a/predict.sh
+++ projects/1a/predict.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# 1st arg - files to send with the job
-# 2nd arg - input path
-# 3rd arg - output path
-# 4th arg - mapper file
 cd ai-masters-bigdata
-#copy input dataset to HDFS
-hdfs dfs -copyFromLocal /datasets/criteo/train-with-id.txt train-with-id.txt
 #remove output dataset if exists
-hdfs dfs -rm -r -f predicted.csv
-projects/tut1/predict.sh projects/tut1/predict.py,projects/tut1/model.py,tut1.joblib hotels.csv predicted.csv predict.py
+hdfs dfs -rm -r -f -skipTrash predicted.csv
+projects/1a/predict.sh projects/1a/predict.py,1a.joblib /datasets/criteo/train-with-id.txt predicted.csv predict.py
