25962068956ab1689b9d260a447d9fd93503acdc
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 02:32:14 +0300

    Update predict.py

--- projects/1a/predict.py
+++ projects/1a/predict.py
@@ -5,19 +5,14 @@ from joblib import load
 from model import fields
 
 def main(model_path, input_file_path):
-    # Загрузка модели
     model = load(model_path)
     
-    # Загрузка данных
     data = pd.read_csv(input_file_path, sep='\t', names=fields)
     
-    # Разделение данных на признаки
     X = data.drop(columns=["id", "label"])
     
-    # Получение предсказаний
     predictions = model.predict_proba(X)[:, 1]
     
-    # Вывод предсказаний
     for pred in predictions:
         print(pred)
 
