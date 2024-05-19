1a119097adaa5d038a804c327ebefb11eaae7fa8
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 02:21:56 +0300

    Update train.py

--- projects/1a/train.py
+++ projects/1a/train.py
@@ -5,17 +5,13 @@ from joblib import dump
 from model import model, fields
 
 def main(project_number, training_file_path):
-    # Загрузка данных
     data = pd.read_csv(training_file_path, sep='\t', names=fields)
     
-    # Разделение данных на признаки и целевую переменную
     X = data.drop(columns=["id", "label"])
     y = data["label"]
     
-    # Обучение модели
     model.fit(X, y)
     
-    # Сохранение модели
     dump(model, f"{project_number}.joblib")
 
 if __name__ == "__main__":
