bfc59315eff72a1786fb3f29cf25c29e2cc99911
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 07:01:03 +0300

    Update Ega2901_dag.py

--- projects/6/Ega2901_dag.py
+++ projects/6/Ega2901_dag.py
@@ -18,7 +18,7 @@ base_dir = '{{ dag_run.conf["base_dir"] if dag_run else "" }}'
 
 feature_eng_train_task = SparkSubmitOperator(
     task_id='feature_eng_train_task',
-    application=f"{base_dir}/feature.py",
+    application=f"{base_dir}/filter.py",
     application_args=["/datasets/amazon/amazon_extrasmall_train.json", "Ega2901_train_out"],
     spark_binary='/usr/bin/spark3-submit',
     num_executors=7,
@@ -36,7 +36,7 @@ download_train_task = BashOperator(
 
 train_task = BashOperator(
     task_id='train_task',
-    bash_command=f'{pyton_path} {base_dir}/train.py {base_dir}/Ega2901_train_out_local {base_dir}/6.joblib',
+    bash_command=f'{pyton_path} {base_dir}/model.py {base_dir}/Ega2901_train_out_local {base_dir}/6.joblib',
     dag=dag
 )
 
@@ -50,7 +50,7 @@ model_sensor = FileSensor(
 
 feature_eng_test_task = SparkSubmitOperator(
     task_id='feature_eng_test_task',
-    application=f"{base_dir}/feature.py",
+    application=f"{base_dir}/filter.py",
     application_args=["/datasets/amazon/amazon_extrasmall_test.json", "Ega2901_test_out"],
     spark_binary='/usr/bin/spark3-submit',
     num_executors=7,
@@ -59,18 +59,6 @@ feature_eng_test_task = SparkSubmitOperator(
     env_vars={'PYSPARK_PYTHON': pyton_path},
     dag=dag
 )
-
-# predict_task = SparkSubmitOperator(
-#     task_id='predict_task',
-#     application=f"{base_dir}/predict.py",
-#     application_args=["Ega2901_test_out", "Ega2901_hw6_prediction", f"{base_dir}/6.joblib"],
-#     spark_binary='/usr/bin/spark3-submit',
-#     num_executors=7,
-#     executor_cores=1,
-#     executor_memory="2G",
-#     env_vars={'PYSPARK_PYTHON': pyton_path},
-#     dag=dag
-# )
 
 predict_task = BashOperator(
     task_id='predict_task',
