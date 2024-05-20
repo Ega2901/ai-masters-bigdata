edeecf2138860022726e769e190614336124b225
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 07:52:27 +0300

    Update train.py

--- projects/4a/train.py
+++ projects/4a/train.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
     model_path = str(sys.argv[2])
     df = spark.read.json(train_path)
     df.cache()
-    df2 =df.na.fill({'overall': 0, 'reviewText': 'unknown'})
     pipeline_model = pipeline.fit(df2)
     pipeline_model.write().overwrite().save(model_path)
     spark.catalog.clearCache()
