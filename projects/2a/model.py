37e93b3dc1be59ff3c40460a372b5b5c5536e1a9
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 07:22:13 +0300

    Update model.py

--- projects/2a/model.py
+++ projects/2a/model.py
@@ -1,37 +1,46 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, log_loss
+import pandas as pd
 
 #
 # Dataset fields
 #
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)]
-
-# without categorical features
 fields = ["id", "label"] + numeric_features + categorical_features
+fields_no_label = ["id"] + numeric_features + categorical_features
 
 #
-# Model pipeline
-#
-
-# We create the preprocessing pipelines for both numeric and categorical data.
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
-#    ('scaler', StandardScaler())
+    ('scaler', StandardScaler())
+])
+
+categorical_features = ['cf1', 'cf3', 'cf4', 'cf6',  'cf10', 'cf11', 'cf13', 'cf14', 'cf15', 'cf17', 'cf20', 'cf22', 'cf23', 'cf25']
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
 ])
 
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
     ]
 )
 
-# Now we have a full prediction pipeline.
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('logregression', LogisticRegression())
+    ('LogisticRegression', LogisticRegression(
+                                              max_iter=10000,
+                                            #   C=0.1,
+                                            #   fit_intercept=True,
+                                            #   penalty='l1',
+                                            #   solver='saga',
+                                            ))
 ])
