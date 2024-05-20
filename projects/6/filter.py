f05281ae37739df386ff7981f9280ea536cb2dc7
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-20 06:54:29 +0300

    Update filter.py

--- projects/6/filter.py
+++ projects/6/filter.py
@@ -1,48 +1,19 @@
-#!/opt/conda/envs/dsenv/bin/python
-import os
-import sys
-SPARK_HOME = "/usr/lib/spark3"
-PYSPARK_PYTHON = "/opt/conda/envs/dsenv/bin/python"
-os.environ["PYSPARK_PYTHON"]= PYSPARK_PYTHON
-os.environ["PYSPARK_DRIVER_PYTHON"]= PYSPARK_PYTHON
-os.environ["SPARK_HOME"] = SPARK_HOME
-PYSPARK_HOME = os.path.join(SPARK_HOME, "python/lib")
-sys.path.insert(0, os.path.join(PYSPARK_HOME, "py4j-0.10.9.5-src.zip"))
-sys.path.insert(0, os.path.join(PYSPARK_HOME, "pyspark.zip"))
-from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
-conf = SparkConf()
-conf.set("spark.ui.port", "4099")
-spark = SparkSession.builder.config(conf=conf).appName("Spark SQL").getOrCreate()                
-from pyspark.sql.types import *
-import pyspark.sql.functions as f
-from pyspark.ml.feature import HashingTF, Tokenizer
-from pyspark.ml import Pipeline
-from pyspark.ml.feature import VectorAssembler
-import pandas as pd
+from pyspark.sql.functions import col, when
+import sys
 
+spark = SparkSession.builder.getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
 
-def filter_df(df):
-    
-    dataset = df.select('id', 'label', 'reviewText')
-    dataset = dataset.na.fill({'label': 0, 'reviewText': 'unknown'})
-    tokenizer = Tokenizer(inputCol = 'reviewText', outputCol = 'tokens')
-    df2 = tokenizer.transform(dataset)
-    
-    hashingTF = HashingTF(numFeatures=30, binary=True, inputCol = 'tokens', outputCol = 'rawFeatures')
-    df2 = hashingTF.transform(df2)
-    df2 = df2.select('id', 'label','rawFeatures')
-    return df2
+path_in = sys.argv[1]
+path_out = sys.argv[2]
 
-if __name__ == "__main__":
-    input_path = str(sys.argv[1])
-    out_path = str(sys.argv[2])
-    df = spark.read.json(input_path)
-    df.cache()
-    df2 = filter_df(df)
-    pandas_df = df2.select(['label', 'rawFeatures']).toPandas()
-    print('OUTPATHHHHHHHHH',out_path)
-    pandas_df.to_csv(f'{out_path}')
-    spark.catalog.clearCache()
-    spark.stop()
+df = spark.read.json(path_in)
+drop_list=['reviewTime', 'reviewerName', 'reviewText', 'summary', 'reviewerID', 'asin']
+df_local = df.drop(*drop_list)
+df_local = df_local.withColumn('vote', col('vote').cast('float'))
+df_local = df_local.withColumn('vote', when(col('vote').isNull(), 0).otherwise(col('vote')))
+df_local.write.json(path_out, mode='overwrite')
 
+#amazon_extrasmall_train.json train_local.json
+#amazon_extrasmall_test.json test_local.json
