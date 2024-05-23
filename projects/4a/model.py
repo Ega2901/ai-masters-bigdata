d03dcdce03478e6bf79b189a89f0f73f6e8b42c5
Egor Tuzharov <105715976+Ega2901@users.noreply.github.com>
2024-05-23 23:57:36 +0300

    Update model.py

--- projects/4a/model.py
+++ projects/4a/model.py
@@ -5,25 +5,56 @@ from pyspark.ml.feature import HashingTF, Tokenizer
 from pyspark.ml import Pipeline, Transformer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.param.shared import Param, Params, TypeConverters
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
-import pyspark.sql.functions as F
+
 
 class CustomImputer(Transformer, DefaultParamsReadable, DefaultParamsWritable):
-    def __init__(self, inputCol=None, outputCol=None, missingValue="missing"):
+    
+    inputCol = Param(Params._dummy(), "inputCol", "The input column", typeConverter=TypeConverters.toString)
+    outputCol = Param(Params._dummy(), "outputCol", "The output column", typeConverter=TypeConverters.toString)
+    fillValue = Param(Params._dummy(), "fillValue", "The value to replace missing values with", typeConverter=TypeConverters.toString)
+
+    def __init__(self, inputCol=None, outputCol=None, fillValue=None):
         super(CustomImputer, self).__init__()
-        self.inputCol = inputCol
-        self.outputCol = outputCol
-        self.missingValue = missingValue
+        self._setDefault(inputCol=None, outputCol=None, fillValue=None)
+        if inputCol is not None:
+            self.setInputCol(inputCol)
+        if outputCol is not None:
+            self.setOutputCol(outputCol)
+        if fillValue is not None:
+            self.setFillValue(fillValue)
 
-    def _transform(self, df: DataFrame) -> DataFrame:
-        if self.inputCol in df.columns:
-            df = df.withColumn(self.outputCol, when(col(self.inputCol).isNull(), lit(self.missingValue)).otherwise(col(self.inputCol)))
-        else:
-            raise ValueError(f"Column '{self.inputCol}' does not exist in DataFrame.")
-        return df
+    def setInputCol(self, value):
+        self._set(inputCol=value)
+        return self
+
+    def setOutputCol(self, value):
+        self._set(outputCol=value)
+        return self
+
+    def setFillValue(self, value):
+        self._set(fillValue=value)
+        return self
+
+    def getInputCol(self):
+        return self.getOrDefault(self.inputCol)
+
+    def getOutputCol(self):
+        return self.getOrDefault(self.outputCol)
+
+    def getFillValue(self):
+        return self.getOrDefault(self.fillValue)
+
+    def _transform(self, dataset: DataFrame) -> DataFrame:
+        input_col = self.getInputCol()
+        output_col = self.getOutputCol()
+        fill_value = self.getFillValue()
+        return dataset.withColumn(output_col, col(input_col).na.fill(fill_value))
 
 
-customImputer = CustomImputer(inputCol='reviewText', outputCol='reviewText_clear', missingValue="missing")
+
+customImputer = CustomImputer(inputCol='reviewText', outputCol='reviewText_clear', fillValue="missing")
 tokenizer = Tokenizer(inputCol = 'reviewText_clear', outputCol = 'tokens')
 hashingTF = HashingTF(numFeatures=100, binary=True, inputCol = 'tokens', outputCol = 'rawFeatures')
 lr = LogisticRegression(featuresCol='rawFeatures',
